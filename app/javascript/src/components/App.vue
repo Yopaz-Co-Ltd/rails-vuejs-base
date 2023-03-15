d389a8c7183909a34b32294e8370627f8d15cb47
HungHvYopaz <hunghv@yopaz.vn>
2023-03-15 16:50:07 +0700

    Add vuejs and bootstrap

---
+++ app/javascript/src/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="m-5">
+    <h5 class="test">Vue</h5>
+    <button-counter></button-counter>
+  </div>
+</template>
+
+<script>
+  import ButtonCounter from "@/src/components/ButtonCounter.vue"
+
+  export default {
+    data() {
+      return {}
+    },
+    components: {
+      ButtonCounter
+    }
+  }
+</script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
+import FullReload from 'vite-plugin-full-reload'
+import vue from '@vitejs/plugin-vue'
+import path from 'path';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
   plugins: [
     RubyPlugin(),
+    FullReload(['config/routes.rb', 'app/views/**/*'], { delay: 200 }),
+    vue()
   ],
 })
